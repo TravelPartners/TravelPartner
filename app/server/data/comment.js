3cba12f054513651d816621845bd5c8bfa404abe
ranshaodedatuimaomao <37781709+ranshaodedatuimaomao@users.noreply.github.com>
2018-04-15 15:58:16 -0400

    comment data schema

--- app/server/data/comment.js
+++ app/server/data/comment.js
@@ -4,7 +4,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let CommentSchema = new Schema({
-    
+  _id: {
+      type: Schema.Types.ObjectId,
+      default: new mongoose.Types.ObjectId
+  },
+  userName: {
+      type: String
+      index: true
+      unique: true
+      required: true
+  },
+  votes: {
+      type: Number
+      required: true
+      default: 0
+  },
+  views: {
+      type:Number
+      required: true
+      default:0
+ },
+ commentBody:{
+      type:String
+      required:true
+ },
+ created_at: {
+     type: Date
+     required:true
+     default: Date.now
+ },
+ keyword:{
+     type:String
+
+ },
+
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
