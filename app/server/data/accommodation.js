df88ffff32534db09893e4da2ee6bf9a9b6cd3e1
ruxin wang <wrxin1230@gmail.com>
2018-04-16 11:28:45 -0400

    update2

--- app/server/data/accommodation.js
+++ app/server/data/accommodation.js
@@ -3,8 +3,38 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let AccommodationSchema = new Schema({
-    
-});
+ let AccommodationSchema = new Schema({
 
-module.exports = mongoose.model('Acco', AccommodationSchema);
+_id: {
+     type: Schema.Types.ObjectId,
+     default: new mongoose.Types.ObjectId
+ },
+name: {
+     type: String,
+     index: true,
+     required: true
+  },
+email: {
+      type: String,
+      required:true;
+
+ },
+phone: {
+      type: String,
+      required:true
+ },
+votes: {
+       type: [String],
+       required: true
+   },
+price:{
+      type:String,
+      required:true
+
+   },
+ img:[String],
+ tags: [String],
+ locations: String
+ });
+
+ module.exports = mongoose.model('Acco', AccommodationSchema);
--- app/server/data/transportation.js
+++ app/server/data/transportation.js
@@ -4,7 +4,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let TransportationSchema = new Schema({
-    
+  _id: {
+      type: Schema.Types.ObjectId,
+      default: new mongoose.Types.ObjectId
+  },
+  name: {
+      type: String,
+      required:true
+  },
+  img:[String],
+  desc:String
 });
 
 module.exports = mongoose.model('Trans', TransportationSchema);
