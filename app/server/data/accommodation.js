2a38bc6c8ffe1972d7c7f9dd2dee45fe9237f13d
ruxin wang <wrxin1230@gmail.com>
2018-04-16 10:29:27 -0400

    update

--- app/server/data/accommodation.js
+++ app/server/data/accommodation.js
@@ -4,7 +4,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let AccommodationSchema = new Schema({
-    
+  _id: {
+      type: Schema.Types.ObjectId,
+      default: new mongoose.Types.ObjectId
+  },
+  name: {
+      type: String,
+      index: true,
+      required: true
+  },
+  email: {
+      type: String,
+  },
+  phone: {
+      type: String,
+  },
+  votes: {
+       type: [String],
+       required: true
+   }
+  img:[String],
+  numbers: [Number],    //price
+  tags: [String],
+  locations: [String]
 });
 
 module.exports = mongoose.model('Acco', AccommodationSchema);
