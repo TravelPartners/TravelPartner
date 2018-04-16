c804bbbabd21700c5067252a01bbab7b2e1db350
Xiaohan Zhang <xiaohanzhang25@hotmail.com>
2018-04-16 17:39:00 -0400

    newpos3
    ,,

--- app/client/app/TG/Home.vue
+++ app/client/app/TG/Home.vue
@@ -1,19 +1,18 @@
 <template>
   <div>
     <h1> {{ message }} </h1>
-    <h2>{{paragraph}}</h2>
+
   </div>
 </template>
 
 <script>
-
 
 export default {
 
   data () {
     return {
       message: 'Home Pag'
-      paragraph: 'woooo'
+
     }
   }
 }
