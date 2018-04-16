49d8f81e002c3f05b430bc082b0db872a3565346
Jason <xsu111@syr.edu>
2018-04-16 02:24:51 +0000

    Add data structure for guide.js

--- app/server/data/guide.js
+++ app/server/data/guide.js
@@ -4,6 +4,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let GuideSchema = new Schema({
+    _id: {
+        type: Schema.Types.ObjectId,
+        default: new mongoose.Types.ObjectId
+    },
+    title: {
+        type:String,
+        index: true,
+        unique: true,
+        required: true
+    },
+    userName: {
+        type: String,
+        index: true,
+        unique: true,
+        required: true
+    },
+    tags: [String],
+    content: {
+        type: String,
+        index: true,
+        unique: true,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
     
 });
 
