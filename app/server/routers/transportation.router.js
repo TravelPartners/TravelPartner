e01afc0022742ae73396f1ee424f5df9a849caa7
ruxin wang <wrxin1230@gmail.com>
2018-04-17 11:16:12 -0400

    update1116am

--- app/server/routers/transportation.router.js
+++ app/server/routers/transportation.router.js
@@ -2,4 +2,24 @@
 
 const router = require('express').Router();
 
+router.get('/TransSearch', (req,res,next)=>{
+
+  let Trans = req.app.locals.db.model('Trans');
+
+  Trans.findOne({ 'name': 'Scene1' }, function (err, trans) {
+    if (err){
+      console.log(err);
+      res.json(err);
+
+    }
+    else{
+      console.log(trans);
+      //res.json(trans);
+      res.render('trans/home');
+    }
+  });
+
+});
+
+
 module.exports = router;
