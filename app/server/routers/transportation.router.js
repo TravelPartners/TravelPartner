afd7421cbeb02d4c070e5d49d3c798684d71d39a
ruxin wang <wrxin1230@gmail.com>
2018-04-18 14:46:19 -0400

    update 246pm

--- app/server/routers/transportation.router.js
+++ app/server/routers/transportation.router.js
@@ -6,18 +6,30 @@ router.get('/TransSearch', (req,res,next)=>{
 
   let Trans = req.app.locals.db.model('Trans');
 
-  Trans.findOne({ 'name': 'Scene1' }, function (err, trans) {
-    if (err){
-      console.log(err);
-      res.json(err);
+  Trans.findOne()
+          .exec()
+          .then((trans) => {
+              let transname = acco.name;
 
-    }
-    else{
-      console.log(trans);
-      //res.json(trans);
-      res.render('trans/home');
-    }
-  });
+              return Trans.find({ 'name': transname})
+                  .then((transs) => {
+                      let ret = [];
+                      for (let Trans of transs) {
+                          ret.push({
+                              image_source:Trans.img,
+                              name: Trans.name
+                          });
+                      }
+                      return ret;
+                  });
+          }).then((transs) => {
+              console.log(transs);
+              res.render('trans/home', { trans: trans });
+          })
+          .catch((err) => {
+              console.log(err);
+              res.sendStatus(500);
+          });
 
 });
 
