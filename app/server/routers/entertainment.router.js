4cd5f34c9ca9b948ed4de8a826c49aa202ef22c8
ranshaodedatuimaomao <37781709+ranshaodedatuimaomao@users.noreply.github.com>
2018-04-19 20:14:46 -0400

    'pp'

--- app/server/routers/entertainment.router.js
+++ app/server/routers/entertainment.router.js
@@ -2,4 +2,53 @@
 
 const router = require('express').Router();
 
+router.get('/:place',(req, res, next)=>{
+  let Place = req.app.locals.db.model('Place');
+  let Spot = req.app.locals.db.model('Spot');
+
+  let place = req.params.place.split('-');
+  let placeName = place[0];
+  let placeId = place[1];
+
+  Place.findById(placeId, function(err, place){
+    if (!err){
+      let spots = place.spots;
+
+      Spot.find({'_id': {$in: spots }}).exec(function(err, result1){
+        console.log(result1);
+        let top = [];
+        let regular =[];
+        for (let result of result1){
+          top.push({
+            img : result.imag,
+            url : '/c/./' + result.name + '-' + result._id,
+            title: result.titel,
+            details: result.details,
+            ticketInfo: result.ticketInfo,
+
+          })
+        };
+        Spot.find({'_id':{$in: spots}}).exec(function(err,result2){
+          for (let result of result2){
+            regular.push({
+              img : result.imag,
+              url : '/c/./' + result.name + '-' + result._id,
+              title: result.titel,
+              details: result.details,
+              ticketInfo: result.ticketInfo,
+
+            })
+          };
+
+        });
+
+
+      });
+//1st
+    }
+
+  });
+
+
+});
 module.exports = router;
